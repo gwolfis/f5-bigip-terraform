5d3ca3268ceb89de3514b1eb39b917512000e92e
gwolfis <g.wolfis@f5.com>
2023-03-08 13:46:17 +0100

    Delete azure-quickstarts/stand-alone directory

--- azure-quickstarts/stand-alone/payg/3nic-bigip-standalone/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output
-
-output "App_Services" {
-  value = <<EOF
-
-      bigip-mgmt       : ${azurerm_network_interface.management.private_ip_address} => https://${azurerm_public_ip.mgmt_pip.ip_address}
-      application-vip1 : https://${element(azurerm_linux_virtual_machine.bigip.public_ip_addresses, 2)}
-    EOF
-}
